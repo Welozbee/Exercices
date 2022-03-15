796ce30edebfce4ed4d44a0f29a16278c59d8c44
Welozbee <90783649+Welozbee@users.noreply.github.com>
2022-03-15 21:20:19 +0100

    Update Syracuse.py
    Suite de Syracuse

--- Syracuse.py
+++ Syracuse.py
@@ -1,19 +1,25 @@
+# Input de l'utilisateur.
 n = int(input("Entrez un entier:"))
+L = 0
 if n > 0:print("Suite de Syracuse pour n={0}".format(n))
-L = 0 
+
+# Loop qui s'arrete quand n = 1.
 while n != 1: 
     L = L+1
-    if n == 0: 
+    if n == 0:  # Pour invalider 0.
         print("Ce nombre est invalide.")
         break
-    elif n%2 == 0: 
+    elif n%2 == 0: # Si le nombre est pair donc on le divise par 2.
         print(n)
         n = n//2
-    else: 
+    else: # Si le Nombre est impair on le multiplie par 3 et on ajout 1.
         print(n)
-        n = 3*n + 1           
+        n = 3*n + 1
+
+# Print le nombre d'opérations + le "1".
 if n == 1: 
     print(1)
     print("Suite terminée en {0} itérations".format(L))
 
-input("Appuyez enter pour sortir de cette fenetre.")
+# Eviter que le programme se ferme directement.
+input("Appuyez Enter pour sortir de cette fenetre.")
